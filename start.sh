72bab325934150a2c916bc7d9d4b848e8246eb04
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2014-04-07 16:04:04 +0300

    update status

--- start.sh
+++ start.sh
@@ -9,6 +9,16 @@ accept="Accept: application/json"
 
 API='http://lotto-dev.lnk.co.ua/api/v1/'
 
+declare -i validator=0
+declare -i counter=0
+function validator
+{
+	counter=$((counter+1))
+	if [[ $1 == *"200 OK"* ]]; then
+		validator=$((validator+1))
+	fi
+} 
+
 printf "\nLogin user\n%suser/login\n" "$API"
 login_string=$(curl -s -H "$accept" -H "$contype"  -c cookie_storage -X POST -d @login {$API}user/login) 
 #-H "$token"
@@ -27,22 +37,31 @@ echo "\033[1m cookie=\033[0m $cookie"
 
 
 printf "\nGet settings\n%ssettings\n" "$API"
-#settings_string=$(curl -I -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X GET {$API}settings)
 settings_string=$(curl -s -I -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X GET {$API}settings | grep -Fi http)
 echo "$settings_string"
+validator "$settings_string"
 
 printf "\nUpdate user \n%suser/%s\n" "$API" "$uid"
 update_user_string=$(curl -s -i -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X PUT -d @userupdate {$API}user/{$uid} | grep -Fi http)
 echo "$update_user_string"
+validator "$update_user_string"
 
 printf "\nGet history user \n%suser/%s/tickets/\n" "$API" "$uid"
 user_tickets_string=$(curl -s -I -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X GET {$API}user/{$uid}/tickets | grep -Fi http)
 echo "$user_tickets_string"
+validator "$user_tickets_string"
 
 printf "\nGet 1 ticket user \n%sticket/497\n" "$API"
 user_1ticket_string=$(curl -s -I -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X GET {$API}ticket/497 | grep -Fi http)
 echo "$user_1ticket_string"
+validator "$user_1ticket_string"
 
+echo "\n"
+if [[ validator == counter ]]; then
+	echo "\033[1m PASSED \033[0m "
+else
+	echo "\033[1m FAILED \033[0m "
+fi
 #printf "\nGet user payment methosds \n%suser/%s/payment_methods\n" "$API" "$uid"
 #user_payment_string=$(curl -s -I -H "$accept" -H "$contype" -H "$token" -b cookie_storage -X GET {$API}user/{$uid}/payment_methods | grep -Fi http)
 #echo "$user_payment_string"
